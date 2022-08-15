dd031a168784fd62c0364921fdb3d243b0b26f93
OmarSabbah999 <omars@mail.tau.ac.il>
2022-08-15 13:03:03 +0300

    final touch -> save2DB

--- pages/api/cars.js
+++ pages/api/cars.js
@@ -4,5 +4,18 @@ import { createCar } from "../../lib/redis"
 export default async function handler(req, res) {
   // req == incoming data from car form component
   const id = await createCar(req.body)
-  res.status(200).json({ id })
+  // .then((dataa) => {
+  //   res.status(200).send("pass")
+  //   console.log(dataa)
+  // })
+  // .catch((error) => {
+  //   res.status(500).send("no pass")
+  //   console.log(error)
+  // })
+  res.status(200).send("pass")
+  console.log(`car id = ${id}`)
+  //   res.status(200).json({ id })
 }
+
+// fetch(serverUrl, { method: "GET" }).then((res) => res.json())
+// res.text()
